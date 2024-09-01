dd66aec361f92442a290c37bb6ff6049f124b00f
Floyd Everest (Monash University) <feverest@monarch-login4.erc.monash.edu>
2024-09-02 01:03:53 +1000

    tweaked scripts and added slurm script to generate results

--- compute_ss.py
+++ compute_ss.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import polars as pl
 import multiprocessing as mp
 
 from numpy.random import SeedSequence, default_rng
@@ -8,11 +9,9 @@ from numpy.random import SeedSequence, default_rng
 from alpha_implementations import new_alpha, old_alpha
 from params import thetas, etas, ds, reps, alpha
 
-experiments = [
-    {"theta": theta, "eta": eta, "d": d, "i": i}
+datasets = [
+    {"theta": theta, "i": i}
     for theta in thetas
-    for eta in etas
-    for d in ds
     for i in range(reps)
 ]
 
@@ -21,20 +20,27 @@ def compute_ss(params):
     print(params)
     theta = params["theta"]
     i = params["i"]
-    eta = params["eta"]
-    d = params["d"]
     in_filename = f"table2_data/theta{theta}_{i}.npz"
-    out_filename = f"table2_results/theta{theta}_eta{eta}_d{d}_{i}.txt"
+    out_filename = f"table2_results/theta{theta}_{i}.csv"
     if os.path.isfile(out_filename):
         # Skip completed computations
         return
     x = np.array(np.load(in_filename)["x"], dtype=np.float64)
-    ss = new_alpha(x, eta, d)
-    print(ss)
-    with open(out_filename, "w") as f:
-        f.write(str(ss))
+    df_eta = []
+    df_d = []
+    df_ss = []
+    for eta in etas:
+        for d in ds:
+            df_eta.append(eta)
+            df_d.append(d)
+            df_ss.append(new_alpha(x, eta, d))
+    pl.DataFrame({
+        "eta": df_eta,
+        "d": df_d,
+        "ss": df_ss
+    }).write_csv(out_filename)
 
 
 if __name__ == "__main__":
     pool = mp.Pool()
-    pool.map(compute_ss, experiments)
+    pool.map(compute_ss, datasets)
